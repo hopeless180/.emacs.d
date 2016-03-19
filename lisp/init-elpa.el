fcf8447e0afa3d47abf492529a7a56615e61aa7f
hopeless180 <474527440@qq.com>
2016-03-19 13:06:29 +0800

    微调

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -24,8 +24,7 @@ re-downloaded in order to locate PACKAGE."
 ;; 强行提前初始化ELPA。因为默认情况下Emacs在init.el加载完之后才开始初始化ELPA，
 ;; 而我们把大多数包的初始化函数都放在init.el中，如果不提前初始化ELPA会导致后面的
 ;; 初始化过程出错（对应的包文件还没有加载进来）。
-
+(setq package-enable-at-startup nil)
 (package-initialize)
-
 (provide 'init-elpa)
 ;; init-elpa.el ends here
