f16f36f3cf14d30c37ce4a85caf22a8f9eab11d4
hopeless180 <474527440@qq.com>
2016-03-19 08:01:31 +0800

    scratch.el
    新建scratch，实现对scratch的个性化设置。因为涉及到频繁修改等问题，因此将它与custom-themes分离单独创建。

---
+++ lisp/scratch.el
@@ -0,0 +1,17 @@
+﻿;;scratch.el
+;;个性化界面
+;;(setq initial-major-mode 'org-mode)
+(setq initial-scratch-message "\
+这是初始界面
+创建文件及打开文件请C-x C-f.
+[[http://goo.gl/g37WPH][个人舰娘排行测试]]
+[[file:c:/Users/阿玖阿玖阿/Documents/java开源框架.org][目前可以公开的java框架]]
+[[file:c:/Users/阿玖阿玖阿/Desktop/简历.org][苦逼的简历]]
+[[file:d:/c++题库.org][c++题库]]
+[[file:d:/java题库.org][java题库]]
+TODO日程表请C-c a a.
+cygwin快捷键是C-M-t
+")
+
+(provide 'scratch)
+;;scratch.el ends here
