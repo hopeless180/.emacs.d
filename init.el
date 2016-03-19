294b21699050be0e870b22ace3597195d7b5de53
hopeless180 <474527440@qq.com>
2016-03-19 13:02:17 +0800

    解决了重复添加package-initialize的问题
    但是为什么呢

--- init.el
+++ init.el
@@ -1,20 +1,25 @@
 ﻿;;init.el
 
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-;;防止乱码
-(set-fontset-font "fontset-default"
-                  'gb18030 '("Microsoft YaHei" . "unicode-bmp"))
-
-;;把lisp添加到搜索路径中
+(setenv "HOME" "d:/emacs")
+(setenv "PATH" "d:/emacs")
+(setq default-directory "~/")
+;;把lisp目录和elpa目录添加到搜索目录中
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "elpa" user-emacs-directory))
+
 ;;elisp文件
 (require 'scratch)
 (require 'custom-themes)
 (require 'init-elpa)
+
+(require 'init-chinese-fonts-setup)
+
 (provide 'init)
 ;;init.el ends here
