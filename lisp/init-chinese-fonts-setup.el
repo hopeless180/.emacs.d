ebbd4b645668831708063b4a46eb213b1a9ff3b6
hopeless180 <474527440@qq.com>
2016-03-19 13:06:14 +0800

    创建CFS初始化文件
    init-chinese-fonts-setup.el

---
+++ lisp/init-chinese-fonts-setup.el
@@ -0,0 +1,8 @@
+;; init-chinese-fonts-setup.el
+(require-package 'chinese-fonts-setup)
+
+(require 'chinese-fonts-setup)
+
+
+(provide 'init-chinese-fonts-setup)
+;; init-chinese-fonts-setup.el ends here
