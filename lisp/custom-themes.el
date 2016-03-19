6119d9c5e69148b5af28d4dba885d064c18b4d14
hopeless180 <474527440@qq.com>
2016-03-19 16:54:28 +0800

    molokai的加载和初始化

--- lisp/custom-themes.el
+++ lisp/custom-themes.el
@@ -1,11 +1,13 @@
 ;;custom-themes.el
+(add-to-list 'custom-theme-load-path "~/.emacs.d/lisp/themes/")
+(setq molokai-theme-kit t)
+(load-theme 'molokai t)
+;; ;;自定义前景背景色
+;; (set-background-color "black") ;; 使用黑色背景
+;; (set-foreground-color "white") ;; 使用白色前景
 
-;;自定义前景背景色
-(set-background-color "black") ;; 使用黑色背景
-(set-foreground-color "white") ;; 使用白色前景
-
-(set-face-foreground 'region "red")  ;; 区域前景颜色设为红色
-(set-face-background 'region "blue") ;; 区域背景色设为蓝色
+;; (set-face-foreground 'region "red")  ;; 区域前景颜色设为红色
+;; (set-face-background 'region "blue") ;; 区域背景色设为蓝色
 
 ;;显示行号
 (global-linum-mode t)
@@ -19,6 +21,26 @@
 ;;美化显示符号, 比如lambda会显示希腊符号
 (prettify-symbols-mode)
 (global-prettify-symbols-mode 1)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c3c0a3702e1d6c0373a0f6a557788dfd49ec9e66e753fb24493579859c8e95ab" default)))
+ '(initial-buffer-choice t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
 
 (provide 'custom-themes)
 ;;custom-themes.el ends here
